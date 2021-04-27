4cf7a18e561029575ec7a76d766892c2bfbe9bde
ayajs <platinumjs0203@gmail.com>
2021-04-27 20:39:26 +0900

    verify email

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,11 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 MEDIA_URL = "/media/"
+
+# Email Configuration
+
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "ayajs@sandboxf922207b7d6f4db29dff75ab6ce415b7.mailgun.org"
